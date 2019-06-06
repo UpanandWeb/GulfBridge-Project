42c40ea4e276377a5979e7988b56418f9fc973d6
UpanandWeb <40310199+UpanandWeb@users.noreply.github.com>
2019-06-06 18:46:22 +0530

    Add files via upload

---
+++ GulfBridgeApp_Live/GulfBridge.Api/Startup.cs
@@ -0,0 +1,40 @@
+﻿using GulfBridge.Api.App_Start;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(GulfBridge.Api.Startup))]
+namespace GulfBridge.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AutoMapperClient.Initialize();
+            ConfigureAuth(app);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            var OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
+                Provider = new AutherizationServerProvider()
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
